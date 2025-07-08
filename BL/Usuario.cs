078a5bac5356c185009c377d312d83c1d2f1427d
Jenifergg25 <jenifergg1@outlook.com>
2025-07-08 14:06:34 -0600

    Add project files.

---
+++ BL/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Usuario
+    {
+        private readonly JgonzalezProgramacionNcapasContext _context;
+
+        public Usuario(JgonzalezProgramacionNcapasContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ PL/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private readonly BL.Usuario _usuario;
+
+        public UsuarioController(BL.Usuario usuario)
+        {
+            _usuario = usuario;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
